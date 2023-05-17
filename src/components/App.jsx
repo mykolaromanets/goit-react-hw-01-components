e2290ac35928b512af09746322574d4550508f92
mykolaromanets <125486364+mykolaromanets@users.noreply.github.com>
2023-05-18 02:04:56 +0300

    ++

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import UserData from '../assets/user.json';
+import DataStatistic from '../assets/data.json';
+// import DataFriends from '../assets/friends.json';
+// import DataTransection from '../assets/transactions.json';
+import { Profile } from '../components/Profile/Profile';
+import { Statistic } from '../components/Statistic/Statistic';
+import { Friends } from '../components/FriendsList/Friends';
+// import { Transection } from '../components/Transetion/Transection';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={UserData.username}
+        tag={UserData.tag}
+        location={UserData.location}
+        avatar={UserData.avatar}
+        stats={UserData.stats}
+      />
+      <Statistic title="Upload stats" stats={DataStatistic} />
+      <Friends />
+    </>
   );
 };
---
+++ src/components/Statistic/Statistic.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Statistic = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+      <ul className="stat-list">
+        {stats.map(({ id, label, percentage }) => (
+          <li key={id} className="item">
+            <span className="label">{label}</span>
+            <span className="percentage">{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistic.defaultProps = {
+  title: '',
+};
+Statistic.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
